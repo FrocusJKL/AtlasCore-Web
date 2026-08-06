c6fb41accfe82872b12a58913e511de14d958bf4
Juan Pablo Gutiérrez Rangel <103089856+FrocusJKL@users.noreply.github.com>
2026-08-05 19:51:50 -0600

    Create Layout page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {Login} from  './features/auth/pages/login/login'
+import { MainLayout } from './layout/main-layout/main-layout';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Login
+        component: Login,
+        children:[
+            
+        ]
     }
 ];
