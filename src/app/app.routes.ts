21946fa4cae5408f0315da62244c9efea6d83ae5
Juan Pablo Gutiérrez Rangel <103089856+FrocusJKL@users.noreply.github.com>
2026-08-08 16:01:23 -0600

    Establish core authentication plumbing and shared utilities
    - Introduce placeholder `AuthGuard` and `AuthInterceptor` for future authentication logic.
    - Add a basic `ApiService` to handle backend interactions.
    - Adjust application routing to nest authenticated routes under `MainLayout`, relocating the `Dashboard` component to fit this structure.
    - Create initial shared components, directives, and pipes: `UiButton`, `Highlight` directive, and `CustomDatePipe`.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
-import {Login} from  './features/auth/pages/login/login'
+import { Login } from  './features/auth/pages/login/login'
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
     {
         path: '',
         component: Login,
-        
+    },
+    {
+        path:'',
+        component: MainLayout,
+        children:[
+            
+        ]
     }
 ];
