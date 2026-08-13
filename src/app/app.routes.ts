a52e696456db00d7f7400dfaa2a4465dffc850a7
Juan Pablo Gutiérrez Rangel <103089856+FrocusJKL@users.noreply.github.com>
2026-08-12 18:35:13 -0600

    refactor(routes): Remove obsolete Dashboard import from app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,15 @@
 import { Routes } from '@angular/router';
-import { Login } from  './features/auth/pages/login/login'
+import { Login } from './features/auth/pages/login/login';
 import { MainLayout } from './layout/main-layout/main-layout';
-import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Login,
-    },
-    {
-        path:'',
-        component: MainLayout,
-        children:[
-            
-        ]
-    }
+  {
+    path: '',
+    component: Login,
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [],
+  },
 ];
