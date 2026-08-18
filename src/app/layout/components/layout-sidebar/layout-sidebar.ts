e0e02933af0a90e298c33148c080c269c25fa2d3
Juan Pablo Gutiérrez Rangel <103089856+FrocusJKL@users.noreply.github.com>
2026-08-18 17:41:36 -0600

    feat(layout): refine sidebar navigation and global styles
    Plaintext
    - Update layout-sidebar HTML, SCSS, and TypeScript files
    - Adjust main-layout markup
    - Add _mixins.scss and update global styles.scss

--- src/app/layout/components/layout-sidebar/layout-sidebar.ts
+++ src/app/layout/components/layout-sidebar/layout-sidebar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'layout-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatListModule, MatIconModule],
   templateUrl: './layout-sidebar.html',
   styleUrl: './layout-sidebar.scss',
 })
